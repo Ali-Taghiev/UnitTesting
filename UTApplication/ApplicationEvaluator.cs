6d0c2cc9a44a5cd909ffc72f7a8b758f2f269570
Ali Taghiev <tagiyev_eli@bk.ru>
2024-02-13 21:03:45 +0400

    Add project files.

---
+++ UTApplication/ApplicationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTApplication.Models;
+
+namespace UTApplication;
+
+public class ApplicationEvaluator
+{
+
+    public ApplicationResult Evaluate(JobApplication form)
+    {
+        return ApplicationResult.AutoAccepted;
+    }
+
+    public enum ApplicationResult
+    {
+        AutoRejected,
+        TransferredToHR,
+        TransferredToLead,
+        TransferredToCTO,
+        AutoAccepted
+    }
+}
---
+++ UTApplication/Models/JobApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTApplication.Models;
+
+public class JobApplication
+{
+    public Applicant Applicant { get; set; }
+    public int YearsOfExperience { get; set; }
+
+    public List<string> TechStackList { get; set; }
+}
